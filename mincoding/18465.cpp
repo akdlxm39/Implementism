9380eb9ac8e356c2caf7099a88b2dc84e0131807
akdlxm39 <zxcwldnd@gmail.com>
2026-02-12 21:10:18 +0900

    solve

--- mincoding/18465.cpp
+++ mincoding/18465.cpp
@@ -3,50 +3,17 @@ using namespace std;
 
 struct Balloon
 {
-    int num;
-    Balloon *left;
-    Balloon *right;
-    int pop()
-    {
-        int ret = 1;
-        if (left != nullptr)
-        {
-            ret *= left->num;
-            left->right = right;
-        }
-        if (right != nullptr)
-        {
-            ret *= right->num;
-            right->left = left;
-        }
-        return ret;
-    }
-    void push(Balloon *b)
-    {
-        if (right != nullptr)
-        {
-            right->left = b;
-        }
-        b->right = right;
-        b->left = this;
-        this->right = b;
-    }
+    int num, left, right;
 };
 
 int ret;
-int n;
-Balloon *head, *tail;
+int n, head;
+Balloon arr[11];
 
 void init()
 {
     ret = 0;
-    for (Balloon *cur = head, *nxt; cur != nullptr; cur = nxt)
-    {
-        nxt = cur->right;
-        delete cur;
-    }
-    head = nullptr;
-    tail = nullptr;
+    head = 0;
 }
 
 void input()
@@ -54,42 +21,60 @@ void input()
     cin >> n;
     for (int i = 0; i < n; ++i)
     {
-        int num;
-        cin >> num;
-        Balloon *new_balloon = new Balloon{num, nullptr, nullptr};
-        if (head == nullptr)
-        {
-            head = tail = new_balloon;
-        }
-        else
-        {
-            new_balloon->left = tail;
-            tail->right = new_balloon;
-            tail = new_balloon;
-        }
+        cin >> arr[i].num;
+        arr[i].left = i - 1;
+        arr[i].right = i + 1;
+    }
+    arr[0].left = arr[n - 1].right = -1;
+}
+
+int pop(int cur)
+{
+    int res = 1;
+    if (arr[cur].left == -1 && arr[cur].right == -1)
+        return arr[cur].num;
+    if (arr[cur].left != -1)
+    {
+        res *= arr[arr[cur].left].num;
+        arr[arr[cur].left].right = arr[cur].right;
+    }
+    else
+        head = arr[cur].right;
+    if (arr[cur].right != -1)
+    {
+        res *= arr[arr[cur].right].num;
+        arr[arr[cur].right].left = arr[cur].left;
+    }
+    return res;
+}
+
+void undo(int cur)
+{
+    if (arr[cur].left != -1)
+    {
+        arr[arr[cur].left].right = cur;
+    }
+    else
+        head = cur;
+    if (arr[cur].right != -1)
+    {
+        arr[arr[cur].right].left = cur;
     }
 }
 
 void dfs(int cnt, int total)
 {
-    if (cnt == n - 1)
+    if (cnt == n)
     {
-        if (ret < total + head->num)
-            ret = total + head->num;
+        if (ret < total)
+            ret = total;
         return;
     }
-    for (Balloon *cur = head->right; cur != nullptr; cur = cur->right)
+    for (int cur = head; cur != -1; cur = arr[cur].right)
     {
-        Balloon *tmp = cur->left;
-        dfs(cnt + 1, total + cur->pop());
-        tmp->push(cur);
+        dfs(cnt + 1, total + pop(cur));
+        undo(cur);
     }
-    Balloon *tmp = head;
-    head = head->right;
-    dfs(cnt + 1, total + tmp->pop());
-    head->left = tmp;
-    tmp->right = head;
-    head = tmp;
 }
 
 void solve()
