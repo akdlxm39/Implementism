22ec800eba06c7425097e167510d733245dfc96b
akdlxm39 <zxcwldnd@gmail.com>
2026-03-27 17:49:28 +0900

    solve

--- SWEA_B/26416/solution.cpp
+++ SWEA_B/26416/solution.cpp
@@ -1,25 +1,249 @@
-#include <cstring>
-// ===================================================
-// SQRT Decomposition
-// ===================================================
+const int INF = 1e9;
 
-// ===================================================
+int n, growthTime[3];
+int grid[1001][1001];
+
+struct Crop
+{
+    int y, x;
+    int sTime;
+    int category;
+    int water;
+    bool isActive;
+} cropPool[100'001];
+int nxtCrop[100'001];
+int cropCnt;
+
+struct Block
+{
+    int crops;
+    int activeCnt;
+    int water;
+    int min[3][30];
+} blocks[32][32];
+
+void rebuild(int by, int bx)
+{
+    Block &block = blocks[by][bx];
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 30; ++j)
+            block.min[i][j] = INF;
+    int cur = block.crops;
+    int prev = 0;
+    while (cur)
+    {
+        if (!cropPool[cur].isActive)
+        {
+            if (prev == 0)
+                block.crops = nxtCrop[cur];
+            else
+                nxtCrop[prev] = nxtCrop[cur];
+        }
+        else
+        {
+            int c = cropPool[cur].category;
+            int g = growthTime[c];
+            int q = cropPool[cur].sTime / g;
+            int r = cropPool[cur].sTime % g;
+            int val = -q + cropPool[cur].water;
+            if (block.min[c][r] > val)
+                block.min[c][r] = val;
+            prev = cur;
+        }
+        cur = nxtCrop[cur];
+    }
+}
 
 void init(int N, int mGrowthTime[])
 {
+    n = N;
+    for (int i = 0; i < 3; ++i)
+        growthTime[i] = mGrowthTime[i];
+
+    cropCnt = 1;
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            grid[i][j] = 0;
+
+    int maxB = N >> 5;
+    for (int i = 0; i <= maxB; ++i)
+    {
+        for (int j = 0; j <= maxB; ++j)
+        {
+            Block &block = blocks[i][j];
+            block.crops = 0;
+            block.activeCnt = 0;
+            block.water = 0;
+            for (int k = 0; k < 3; ++k)
+                for (int l = 0; l < 30; ++l)
+                    block.min[k][l] = INF;
+        }
+    }
 }
 
 int sow(int mTime, int mRow, int mCol, int mCategory)
 {
-    return 0;
+    if (grid[mRow][mCol])
+        return 0;
+    int id = cropCnt++;
+    Block &block = blocks[mRow >> 5][mCol >> 5];
+
+    cropPool[id] = {mRow, mCol, mTime, mCategory, -block.water, true};
+    grid[mRow][mCol] = id;
+    nxtCrop[id] = block.crops;
+    block.crops = id;
+    block.activeCnt++;
+
+    int g = growthTime[mCategory];
+    int q = mTime / g;
+    int r = mTime % g;
+    int val = -q + cropPool[id].water;
+    if (block.min[mCategory][r] > val)
+        block.min[mCategory][r] = val;
+    return 1;
 }
 
 int water(int mTime, int G, int mRow, int mCol, int mHeight, int mWidth)
 {
-    return 0;
+    int y1 = mRow, y2 = mRow + mHeight - 1;
+    int x1 = mCol, x2 = mCol + mWidth - 1;
+    int by1 = y1 >> 5, by2 = y2 >> 5;
+    int bx1 = x1 >> 5, bx2 = x2 >> 5;
+    int ret = 0;
+    for (int by = by1; by <= by2; ++by)
+    {
+        for (int bx = bx1; bx <= bx2; ++bx)
+        {
+            bool fullY = (y1 <= by * 32) && (by * 32 + 31 <= y2);
+            bool fullX = (x1 <= bx * 32) && (bx * 32 + 31 <= x2);
+            if (fullY && fullX)
+            {
+                blocks[by][bx].water += G;
+                ret += blocks[by][bx].activeCnt;
+            }
+            else
+            {
+                bool updated = false;
+                int cur = blocks[by][bx].crops;
+                while (cur)
+                {
+                    if (cropPool[cur].isActive)
+                    {
+                        int cy = cropPool[cur].y, cx = cropPool[cur].x;
+                        if (y1 <= cy && cy <= y2 && x1 <= cx && cx <= x2)
+                        {
+                            cropPool[cur].water += G;
+                            ret++;
+                            updated = true;
+                        }
+                    }
+                    cur = nxtCrop[cur];
+                }
+                if (updated)
+                    rebuild(by, bx);
+            }
+        }
+    }
+    return ret;
 }
 
 int harvest(int mTime, int L, int mRow, int mCol, int mHeight, int mWidth)
 {
-    return 0;
+    int y1 = mRow, y2 = mRow + mHeight - 1;
+    int x1 = mCol, x2 = mCol + mWidth - 1;
+    int by1 = y1 >> 5, by2 = y2 >> 5;
+    int bx1 = x1 >> 5, bx2 = x2 >> 5;
+    for (int by = by1; by <= by2; ++by)
+    {
+        for (int bx = bx1; bx <= bx2; ++bx)
+        {
+            if (blocks[by][bx].activeCnt == 0)
+                continue;
+            bool fullY = (y1 <= by * 32) && (by * 32 + 31 <= y2);
+            bool fullX = (x1 <= bx * 32) && (bx * 32 + 31 <= x2);
+            if (fullY && fullX)
+            {
+                for (int c = 0; c < 3; ++c)
+                {
+                    int g = growthTime[c];
+                    int q = mTime / g;
+                    int r = mTime % g;
+                    for (int rp = 0; rp < g; ++rp)
+                        if (q + blocks[by][bx].min[c][rp] + blocks[by][bx].water - (r < rp) < L)
+                            return 0;
+                }
+            }
+            else
+            {
+                int cur = blocks[by][bx].crops;
+                while (cur)
+                {
+                    if (cropPool[cur].isActive)
+                    {
+                        int cy = cropPool[cur].y, cx = cropPool[cur].x;
+                        if (y1 <= cy && cy <= y2 && x1 <= cx && cx <= x2)
+                        {
+                            int c = cropPool[cur].category;
+                            int g = growthTime[c];
+                            if ((mTime - cropPool[cur].sTime) / g + cropPool[cur].water + blocks[by][bx].water < L)
+                                return 0;
+                        }
+                    }
+                    cur = nxtCrop[cur];
+                }
+            }
+        }
+    }
+
+    int ret = 0;
+    for (int by = by1; by <= by2; ++by)
+    {
+        for (int bx = bx1; bx <= bx2; ++bx)
+        {
+            if (blocks[by][bx].activeCnt == 0)
+                continue;
+            bool fullY = (y1 <= by * 32) && (by * 32 + 31 <= y2);
+            bool fullX = (x1 <= bx * 32) && (bx * 32 + 31 <= x2);
+            if (fullY && fullX)
+            {
+                ret += blocks[by][bx].activeCnt;
+                int cur = blocks[by][bx].crops;
+                while (cur)
+                {
+                    if (cropPool[cur].isActive)
+                    {
+                        grid[cropPool[cur].y][cropPool[cur].x] = 0;
+                        cropPool[cur].isActive = false;
+                    }
+                    cur = nxtCrop[cur];
+                }
+                blocks[by][bx].activeCnt = 0;
+                rebuild(by, bx);
+            }
+            else
+            {
+                bool updated = false;
+                int cur = blocks[by][bx].crops;
+                while (cur)
+                {
+                    if (cropPool[cur].isActive)
+                    {
+                        int cy = cropPool[cur].y, cx = cropPool[cur].x;
+                        if (y1 <= cy && cy <= y2 && x1 <= cx && cx <= x2)
+                        {
+                            grid[cy][cx] = 0;
+                            cropPool[cur].isActive = false;
+                            ret++;
+                            blocks[by][bx].activeCnt--;
+                            updated = true;
+                        }
+                    }
+                    cur = nxtCrop[cur];
+                }
+                if (updated)
+                    rebuild(by, bx);
+            }
+        }
+    }
+    return ret;
 }
--- test.cpp
+++ test.cpp
@@ -1,35 +1,112 @@
-#include <iostream>
-#include <unordered_map>
-using namespace std;
+#include <memory.h>
 
-int main()
+#define FR(i, s, e) for (register int i = (s); i <= (e); ++i)
+#define WH(c) while ((c) != -1)
+#define GP grain_pool
+#define BH bucket_head
+#define BW bucket_water
+#define BA bucket_alive
+#define RN mRow, (mRow + mHeight - 1), mCol, (mCol + mWidth - 1)
+struct G
 {
-    unordered_map<int, pair<int, int>> map;
-    map[1] = {4, 6};
-    map[5] = {11, 11};
-
-    // 얘는 같은 iterator 같지만, 실제로는 list<pair>의 iterator
-    for (auto it : map)
+    int y, x, c, s, t, p, n;
+} GP[100005];
+int BH[11][11], BW[11][11], BA[11][11], V[1000][1000], GT[3], GC, BS;
+inline bool II(int y, int x, int ly, int ry, int lx, int rx) { return y >= ly && y <= ry && x >= lx && x <= rx; }
+inline int GS(int ct, int t, int c) { return (ct - t) / GT[c]; }
+void init(int N, int mGT[])
+{
+    FR(i, 0, 2)
+    GT[i] = mGT[i];
+    GC = 0;
+    BS = (N == 10) ? 1 : (N <= 100 ? 10 : 100);
+    memset(V, 0, sizeof(V));
+    memset(BW, 0, sizeof(BW));
+    memset(BA, 0, sizeof(BA));
+    memset(BH, -1, sizeof(BH));
+}
+int sow(int mTime, int mRow, int mCol, int mCategory)
+{
+    if (V[mRow][mCol])
+        return 0;
+    V[mRow][mCol] = 1;
+    int y = mRow / BS, x = mCol / BS, h = BH[y][x];
+    GP[GC] = {mRow, mCol, mCategory, -BW[y][x], mTime, -1, h};
+    if (h != -1)
+        GP[h].p = GC;
+    BH[y][x] = GC++;
+    BA[y][x]++;
+    return 1;
+}
+int water(int mTime, int Gv, int mRow, int mCol, int mHeight, int mWidth)
+{
+    int by = mRow / BS, bx = mCol / BS, ey = (mRow + mHeight - 1) / BS, ex = (mCol + mWidth - 1) / BS, res = 0;
+    FR(sy, by, ey)
+    FR(sx, bx, ex)
     {
-        cout << typeid(it).name() << '\n';
-
-        cout << "key : " << it.first << '\n';
-        cout << "pair.first : " << it.second.first << '\n';
-        cout << "pair.second : " << it.second.second << '\n';
-        cout << '\n';
+        int l = sy * BS, r = l + BS - 1, a = sx * BS, b = a + BS - 1;
+        if (mRow <= l && mRow + mHeight - 1 >= r && mCol <= a && mCol + mWidth - 1 >= b)
+        {
+            BW[sy][sx] += Gv;
+            res += BA[sy][sx];
+        }
+        else
+        {
+            int c = BH[sy][sx];
+            WH(c)
+            {
+                if (II(GP[c].y, GP[c].x, RN))
+                {
+                    GP[c].s += Gv;
+                    ++res;
+                }
+                c = GP[c].n;
+            }
+        }
     }
-
-    // hashmap == iter를 pair로 관리해
-    // 근데 아래는 iter가 그 페어의 주소를 가지고 있어
-    // pair가 주르르르륵 있고, 그거를 "주소로" 순회하는 애;
-    for (auto it = map.begin(); it != map.end(); ++it)
+    return res;
+}
+int harvest(int mTime, int L, int mRow, int mCol, int mHeight, int mWidth)
+{
+    int by = mRow / BS, bx = mCol / BS, ey = (mRow + mHeight - 1) / BS, ex = (mCol + mWidth - 1) / BS, res = 0;
+    FR(sy, by, ey)
+    FR(sx, bx, ex)
     {
-        cout << typeid(it).name() << '\n';
-        cout << "key : " << it->first << '\n';
-        cout << "pair.first : " << it->second.first << '\n';
-        cout << "pair.second : " << it->second.second << '\n';
-        cout << '\n';
+        int c = BH[sy][sx];
+        WH(c)
+        {
+            if (II(GP[c].y, GP[c].x, RN))
+            {
+                if (GS(mTime, GP[c].t, GP[c].c) + GP[c].s + BW[sy][sx] < L)
+                    return 0;
+                ++res;
+            }
+            c = GP[c].n;
+        }
     }
-
-    return 0;
+    if (!res)
+        return 0;
+    FR(sy, by, ey)
+    FR(sx, bx, ex)
+    {
+        int c = BH[sy][sx];
+        WH(c)
+        {
+            int nt = GP[c].n;
+            if (II(GP[c].y, GP[c].x, RN))
+            {
+                V[GP[c].y][GP[c].x] = 0;
+                BA[sy][sx]--;
+                int p = GP[c].p, n = GP[c].n;
+                if (p != -1)
+                    GP[p].n = n;
+                else
+                    BH[sy][sx] = n;
+                if (n != -1)
+                    GP[n].p = p;
+            }
+            c = nt;
+        }
+    }
+    return res;
 }
