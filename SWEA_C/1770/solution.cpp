5e8c84082e38863067c169066c4ef4a70e365bee
dndwlqor <dndwlqor@gmail.com>
2026-04-16 17:12:44 +0900

    solve

---
+++ SWEA_C/1770/solution.cpp
@@ -0,0 +1,9 @@
+struct block
+{
+    int base;
+    long long mask;
+};
+
+int makeBlock(int module[][4][4])
+{
+}
--- test.cpp
+++ test.cpp
@@ -1,60 +1,22 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int n, m, ans;
-string map_[600];
-bool visited[600][600];
-
-struct Point
-{
-    int y, x;
-    Point operator+(Point other) const
-    {
-        return {y + other.y, x + other.x};
-    }
-    bool isValid() const
-    {
-        return 0 <= y && y < n && 0 <= x && x < m && map_[y][x] != 'X' && !visited[y][x];
-    }
-};
-
-const Point DIR[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, len, max_len;
+int tanghuru[200'000];
+int fruit[10];
 
 void solve()
 {
-    cin >> n >> m;
-    queue<Point> q;
+    cin >> n;
     for (int i = 0; i < n; ++i)
+        cin >> tanghuru[i];
+    int l = 0, r = 0, cnt = 0;
+    while (r <= n)
     {
-        cin >> map_[i];
-        size_t j = map_[i].find('I');
-        if (j != string::npos)
-        {
-            q.push({i, int(j)});
-            visited[i][j] = true;
-        }
+        fruit[tanghuru[r++]]++;
     }
-    while (!q.empty())
-    {
-        Point cur = q.front();
-        q.pop();
-        if (map_[cur.y][cur.x] == 'P')
-            ans++;
-        for (Point d : DIR)
-        {
-            Point nxt = cur + d;
-            if (nxt.isValid())
-            {
-                q.push(nxt);
-                visited[nxt.y][nxt.x] = true;
-            }
-        }
-    }
-    cout << (ans ? to_string(ans) : "TT") << '\n';
 }
 
 int main()
