03b2b9a62b2d1d861d3ae80c43bccf622cc2628f
dndwlqor <dndwlqor@naver.com>
2026-05-06 15:31:41 +0900

    solve

--- test.cpp
+++ test.cpp
@@ -1,20 +1,7 @@
-#include <string>
-#include <vector>
-#include <algorithm>
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-    int n = 1561881131;
-    string s;
-    while (n)
-    {
-        s += (n % 10 + '0');
-        n /= 10;
-    }
-    reverse(s.begin(), s.end());
-    cout << s << endl;
-    return 0;
+    cout << "Hello World!" << endl;
 }
