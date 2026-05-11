efbf299e8d92d185d5f3cbdab7ac25f32c438a1e
akdlxm39 <zxcwldnd@gmail.com>
2026-05-12 00:24:35 +0900

    solve

---
+++ SWEA_B/24997/solution.cpp
@@ -0,0 +1,35 @@
+struct Result
+{
+    int mX, mY;
+    int mMoveDistance;
+    int mRideDistance;
+};
+
+struct Taxi
+{
+    int y, x;
+};
+
+void init(int N, int M, int L, int mXs[], int mYs[])
+{
+    return;
+}
+
+int pickup(int mSX, int mSY, int mEX, int mEY)
+{
+    return -1;
+}
+
+Result reset(int mNo)
+{
+    Result res;
+
+    res.mX = res.mY = res.mMoveDistance = res.mRideDistance = -1;
+
+    return res;
+}
+
+void getBest(int mNos[])
+{
+    return;
+}
