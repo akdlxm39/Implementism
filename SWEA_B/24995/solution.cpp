ebdd1506a4c79c901a9f712339aa0f14ff26bff7
akdlxm39 <dndwlqor@naver.com>
2026-05-14 14:25:53 +0900

    solve

---
+++ SWEA_B/24995/solution.cpp
@@ -0,0 +1,34 @@
+struct RESULT
+{
+    int cnt;
+    int IDs[5];
+};
+
+void init()
+{
+    return;
+}
+
+int sell(int mID, int mCategory, int mCompany, int mPrice)
+{
+    return -1;
+}
+
+int closeSale(int mID)
+{
+    return -1;
+}
+
+int discount(int mCategory, int mCompany, int mAmount)
+{
+    return -1;
+}
+
+RESULT show(int mHow, int mCode)
+{
+    RESULT res;
+
+    res.cnt = -1;
+
+    return res;
+}
